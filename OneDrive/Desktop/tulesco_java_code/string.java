87dcf5da644d40e44e82a9c70e8d274f3c5b9911
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2025-09-13 10:34:01 +0530

    java tulesco code:

---
+++ OneDrive/Desktop/tulesco_java_code/string.java
@@ -0,0 +1,10 @@
+public class string
+{
+    public static void main(String args[])
+    {
+        String name="chirag";
+        String nam=new String("hash");
+        // both are methods to use string
+        System.out.println("hello " + name + nam);
+    }
+}
